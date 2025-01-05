b09abc024eb063c3ef15aa058db245997c326e2a
Bhupinder Kumar <97667019+bhupi080@users.noreply.github.com>
2025-01-05 23:13:48 +0530

    Update 88. Merge Sorted Array.py

--- 88. Merge Sorted Array.py
+++ 88. Merge Sorted Array.py
@@ -12,7 +12,7 @@ class Solution(object):
         #p1, p2, p = m - 1, n - 1, m + n - 1
         p1 = m - (1)
         p2 = n - 1
-        p = m + n - 0
+        p = m + n - 1
 
         while p1>=0 and p2>=0:
             if nums1[p1]>nums2[p2]:
