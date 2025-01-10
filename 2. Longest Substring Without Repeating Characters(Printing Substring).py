dc4648fa5a6fff503658768439f31c628439ea2c
Bhupinder Kumar <97667019+bhupi080@users.noreply.github.com>
2025-01-10 22:18:07 +0530

    Update 2. Longest Substring Without Repeating Characters(Printing Substring).py

--- 2. Longest Substring Without Repeating Characters(Printing Substring).py
+++ 2. Longest Substring Without Repeating Characters(Printing Substring).py
@@ -4,7 +4,7 @@ def longest_substring(string: str) -> int:
     start = 0
     max_length = 0
 
-    for i in range(len(string)):
+    for i in range(len(string))
         if string[i] in dic:
             start = max(start,dic[string[i]]+1)
         dic[string[i]] = i
